c4a4936e1d3152d5067021be45790cf456d2f3a7
Anush <afr741@mun.ca>
2019-07-28 21:25:36 -0230

    added fullpage version in seperate folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 let anchorlinks = document.querySelectorAll('a[href^="#"]')
 
-for (let item of anchorlinks) { // relitere 
+for (let item of anchorlinks) { // relitere
     item.addEventListener('click', (e)=> {
         let hashval = item.getAttribute('href')
         let target = document.querySelector(hashval)
@@ -17,3 +17,39 @@ var c=new Image("../images/blogbg.jpeg");
 c.onload=function(){
    //render the form
 }
+
+
+
+
+
+let mainNavLinks = document.querySelectorAll("header ul li a");
+let mainSections = document.querySelectorAll("div section");
+
+let lastId;
+let cur = [];
+
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
