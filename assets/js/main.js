4afff2516e6cd18c888b17aa244c4c059c97855f
Anush <afr741@mun.ca>
2019-08-21 22:05:28 -0230

    weird changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 let anchorlinks = document.querySelectorAll('a[href^="#"]')
-
+console.log("js is loading again");
 for (let item of anchorlinks) { // relitere
     item.addEventListener('click', (e)=> {
         let hashval = item.getAttribute('href')
@@ -8,48 +8,6 @@ for (let item of anchorlinks) { // relitere
             behavior: 'smooth'
         })
         history.pushState(null, null, hashval)
-        e.preventDefault()
+        e.preventDefault();
     })
 }
-
-
-var c=new Image("../images/blogbg.jpeg");
-c.onload=function(){
-   //render the form
-}
-
-
-
-
-
-let mainNavLinks = document.querySelectorAll("header ul li a");
-let mainSections = document.querySelectorAll("div section");
-
-let lastId;
-let cur = [];
-
-// This should probably be throttled.
-// Especially because it triggers during smooth scrolling.
-// https://lodash.com/docs/4.17.10#throttle
-// You could do like...
-// window.addEventListener("scroll", () => {
-//    _.throttle(doThatStuff, 100);
-// });
-// Only not doing it here to keep this Pen dependency-free.
-
-window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
-
-  mainNavLinks.forEach(link => {
-    let section = document.querySelector(link.hash);
-
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add("current");
-    } else {
-      link.classList.remove("current");
-    }
-  });
-});
